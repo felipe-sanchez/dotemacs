8471c0c32a262b51d87b975f435e292b1610ce54
Stefano Meschiari <stefano.meschiari@gmail.com>
2014-01-08 12:03:10 -0600

    Added new utility for keeping track of git repos status

--- .bash_profile
+++ .bash_profile
@@ -4,13 +4,20 @@ function parse_git_dirty {
 function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
 }
-export PS1='\u \[\033[1;33m\]\w\[\033[0m\]$(parse_git_branch)$ '
+
 
 alias aquamacs='open -b org.gnu.Aquamacs'
 # MacPorts Installer addition on 2012-10-01_at_17:56:59: adding an appropriate PATH variable for use with MacPorts.
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 # Finished adapting your PATH environment variable for use with MacPorts.
 
+# Launches git global daemon, if needed
+ps aux | grep git_global_status.lua | grep -v grep >/dev/null
+if [ $? -eq 1 ]; then
+    echo "Launching git_global_status daemon"
+    cd ~/Projects/dotemacs/utils; /usr/bin/nohup >/dev/null 2>&1 lua ~/Projects/dotemacs/utils/git_global_status.lua &
+    cd ~
+fi
 
 # added by Anaconda 1.6.1 installer
 export PATH="$HOME/anaconda/bin:$PATH"
@@ -18,6 +25,9 @@ export PATH="$HOME/local/emsdk_portable:$HOME/local/emsdk_portable/emscripten/in
 export PATH="/usr/texbin/:$PATH"
 export C_INCLUDE_PATH="/opt/local/include"
 alias emacs="emacsclient -n "
+alias git_status="cat ~/.git_global_report"
+
+export PS1='`cat ~/.git_global_stats` \u \[\033[1;33m\]\w\[\033[0m\]$(parse_git_branch)$ '
 
 export XGRID_CONTROLLER_HOSTNAME="xgrid.as.utexas.edu"
 export XGRID_CONTROLLER_PASSWORD="ast_xgrid"
---
+++ utils/git_global_status.lua
@@ -0,0 +1,89 @@
+-- (c) 2014, Stefano Meschiari
+--
+-- Simple "daemon" that scans all git repositories and checks whether
+-- (a) there are repositories that have changes to be committed, and
+-- (b) there are repositories whose remote is ahead of the current version.
+--
+-- A repository statistics in the form [total/needs_commit/needs_pull] and a detailed
+-- report is saved in ~/.git_global_stats and ~/.git_global_report, respectively.
+--
+-- For convenience, I put "[`cat ~/.git_global_stats`]" as part of my bash prompt to
+-- check the status of my git repositories, and an alias git_global="cat ~/.git_global_report | less" to
+-- see a list of repos and their status.
+--
+-- By default, check every minute.
+require 'utils'
+
+local capture = ex.capture
+local capturelines = ex.capturelines
+local fprintf = ex.fprintf
+local home = os.getenv("HOME")
+local defaultdir = home .. "/Projects/"
+local dirs = arg
+if not dirs[1] then
+   dirs[1] = defaultdir
+end
+
+
+local git = {}
+git.clean = {}
+git.needscommit = {}
+git.uptodate = {}
+git.needspull = {}
+
+function git.repos(dir)
+   return capturelines("cd " .. dir .. "; find . -name .git -print")
+end
+
+function git.status(dir)
+   local st = capture("cd " .. dir .. "; git status 2>&1 -s")
+
+   if (st == "") then
+      return git.clean, st
+   else
+      return git.needscommit, st
+   end
+end
+
+function git.remotestatus(dir)
+   local st = capture("cd " .. dir .. "; git pull 2>&1 -v --dry-run")
+   if (st:find("up to date")) then
+      return git.clean, st
+   end
+end
+
+while true do
+   local b = buf.buffer()
+   local n = 0
+   local nneedscommit = 0
+   local nneedspull = 0
+   
+   for _, dir in ipairs(dirs) do
+      for _, repo in ipairs(git.repos(dir)) do
+         local name = repo:match("([^%./]+)/.git$")
+         local repodir = dir .. "/" .. repo:match("^(.+).git$")
+         local st, stret = git.status(repodir)
+         local rst, rstret = git.remotestatus(repodir)
+         n = n + 1
+         if st == git.needscommit then
+            nneedscommit = nneedscommit + 1
+         end
+         if rst == git.needspull then
+            nneedspull = nneddspull + 1
+         end
+
+         if st == git.needscommit or st == git.needspull then
+            b:append(name, "\n", string.rep("=", name:len()), "\n")
+            b:append(st == git.needscommit and stret or "No file changed/added.\n",
+                  st == git.needspull and rstret or "No pull needed.\n", "\n")         
+         end
+      end
+   end
+
+   local f = io.open(home .. "/.git_global_stats", "w")
+   fprintf(f, "[%d/c%d/r%d]", n, nneedscommit, nneedspull)
+   f:close()
+
+   b:save(home .. "/.git_global_report")
+   os.execute("sleep 30")
+end
