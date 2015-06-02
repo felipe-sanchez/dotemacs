378882fe0fdf9798e31bf9428e8ad62a8e799787
Stefano Meschiari <stefano.meschiari@gmail.com>
2015-06-02 11:35:26 -0500

    Modified prompt and git opts for fish

--- config.fish
+++ config.fish
@@ -105,3 +105,42 @@ end
 # complete conda environment names when activating
 complete -c condactivate -xA -a "(condalist)"
 complete -c ca -xA -a "(condalist)"
+
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '*'
+set __fish_git_prompt_char_stagedstate '>'
+set __fish_git_prompt_char_untrackedfiles 'U'
+set __fish_git_prompt_char_stashstate 'S'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+function fish_prompt --description 'Write out the prompt'
+	
+	set -l last_status $status
+
+	if not set -q __fish_prompt_normal
+		set -g __fish_prompt_normal (set_color normal)
+	end
+
+	# PWD
+	set_color $fish_color_cwd
+	echo -n (prompt_pwd)
+	set_color normal
+
+	printf '%s ' (__fish_git_prompt)
+
+	if not test $last_status -eq 0
+	set_color $fish_color_error
+	end
+
+	echo -n '▶ '
+
+end
